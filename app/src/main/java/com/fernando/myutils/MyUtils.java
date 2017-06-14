52ab2eea2edb4f0e4a4500f2fa1040acc669d207
Fernando Roberto González Tapia <fgonzalez@calymayor.com.mx>
2017-06-14 13:48:09 -0500

    Adding more methods

--- app/src/main/java/com/fernando/myutils/MyUtils.java
+++ app/src/main/java/com/fernando/myutils/MyUtils.java
@@ -1,9 +1,28 @@
 package com.fernando.myutils;
 
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Base64;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 /**
  * Created by fgonzalez on 14/06/2017.
@@ -24,5 +43,128 @@ public class MyUtils {
                 cursor.close();
             }
         }
+    }
+
+    public static String createDirectoryAndSaveFile(Bitmap imageToSave, String fileName, String dirName) {
+        File direct = new File(Environment.getExternalStorageDirectory() + File.separator + dirName);
+        //File direct = new File(Environment.getDataDirectory() + "/DirName");
+
+        if (!direct.exists()) {
+            File wallpaperDirectory = new File(Environment.getExternalStorageDirectory() +
+                    File.separator + dirName + File.separator);
+            wallpaperDirectory.mkdirs();
+        }
+
+        File file = new File(new File(Environment.getExternalStorageDirectory() +
+                File.separator + dirName + File.separator), fileName);
+        if (file.exists()) {
+            file.delete();
+        }
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            imageToSave.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            out.flush();
+            out.close();
+            return file.getAbsolutePath();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String bitmapToBase64String(Bitmap bmp){
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+        String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+
+        return encodedImage;
+    }
+
+    public static void makeDatePicker(final EditText editText) {
+        //editText.setText(Utils.getCurrentDate());
+        editText.setFocusable(false);
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar myCalendar = Calendar.getInstance();
+
+                DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        myCalendar.set(Calendar.YEAR, year);
+                        myCalendar.set(Calendar.MONTH, monthOfYear);
+                        myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                        String myFormat = "yyyyMMdd";
+                        final SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+                        editText.setText(sdf.format(myCalendar.getTime()));
+                    }
+                };
+
+                new DatePickerDialog(editText.getContext(), date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+
+    public static void makeDateTimePicker(final EditText editText) {
+        //editText.setText(Utils.getCurrentDate());
+        editText.setFocusable(false);
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar myCalendar = Calendar.getInstance();
+
+                DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        myCalendar.set(Calendar.YEAR, year);
+                        myCalendar.set(Calendar.MONTH, monthOfYear);
+                        myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                        String myFormat = "yyyyMMdd HH:mm:ss";
+                        final SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+                        int hour = myCalendar.get(Calendar.HOUR_OF_DAY);
+                        int minute = myCalendar.get(Calendar.MINUTE);
+                        TimePickerDialog mTimePicker;
+                        mTimePicker = new TimePickerDialog(view.getContext(), new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                                myCalendar.set(Calendar.HOUR_OF_DAY, selectedHour);
+                                myCalendar.set(Calendar.MINUTE, selectedMinute);
+                                editText.setText(sdf.format(myCalendar.getTime()));
+                            }
+                        }, hour, minute, false);
+                        mTimePicker.show();
+                    }
+                };
+
+                new DatePickerDialog(editText.getContext(), date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+
+    public static void startImageChoosingIntent(Context context, Uri imageUri, int requestCode) {
+        Intent pickIntent = new Intent();
+        pickIntent.setType("image/*");
+        pickIntent.setAction(Intent.ACTION_GET_CONTENT);
+
+        Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        String pickTitle = "Seleccionar";
+
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "New Picture");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From your Camera");
+        imageUri = context.getContentResolver().insert(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+
+        Intent chooserIntent = Intent.createChooser(pickIntent, pickTitle);
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[]{takePhotoIntent});
+
+        ((Activity)context).startActivityForResult(chooserIntent, requestCode);
     }
 }
